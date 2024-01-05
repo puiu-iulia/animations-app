a133cfc1a35bb9b163892e73000f23896e7a9a09
Iulia Puiu <iulia.puiu@hotmail.com>
2024-01-05 14:02:52 +0200

    Animate styles with SharedValues

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,13 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { HomePageProps } from '../navigation/RootStack'
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
 
-const Home = ({navigation} : HomePageProps) => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+const Home = ({ navigation }: HomePageProps) => {
+
+    const width = useSharedValue(100)
+    const height = useSharedValue(100)
+    const backgroundColor = useSharedValue('red')
+
+    const startAnimation = () => {
+        width.value = 200
+        height.value = 200
+        backgroundColor.value = 'blue'
+    }
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            width: width.value,
+            height: height.value,
+            backgroundColor: backgroundColor.value
+        }
+    })
+
+    return (
+        <View>
+            <Animated.View style={animatedStyles} />
+            <Button title="Start Animation" onPress={startAnimation} />
+        </View>
+    )
 }
 
 export default Home
