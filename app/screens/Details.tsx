c9a62ebe634fd64cb53008ee812c7f1988553dbd
Iulia Puiu <iulia.puiu@hotmail.com>
2024-01-06 15:27:55 +0200

    Implement SharedElementTransition

--- app/screens/Details.tsx
+++ app/screens/Details.tsx
@@ -1,12 +1,105 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React, { useState, useLayoutEffect } from 'react'
+import { DetailsPageProps } from '../navigation/RootStack'
+import Animated, {
+    FadeIn,
+    FadeInLeft,
+    FadeOut,
+    LightSpeedInLeft,
+    SlideInDown,
+    SlideOutDown,
+    useAnimatedStyle,
+    useSharedValue,
+} from 'react-native-reanimated'
+import { sharedElementTransition } from '../utils/SharedElementTransition';
 
-const Details = () => {
-  return (
-    <View>
-      <Text>Details</Text>
-    </View>
-  )
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+const Details = ({ route, navigation }: DetailsPageProps) => {
+
+    const { item } = route.params;
+    const [isOpen, setIsOpen] = useState(false);
+    const offset = useSharedValue(0);
+
+    // useLayoutEffect(() => {
+    //     navigation.setOptions({
+    //         headerTitle: item.title,
+    //         headerRight: () => (
+    //             <Pressable onPress={onToggle}>
+    //                 <Text style={{ marginRight: 16 }}>Open</Text>
+    //             </Pressable>
+    //         ),
+    //     });
+    // });
+
+    const onToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const translateY = useAnimatedStyle(() => ({
+        transform: [{ translateY: offset.value }],
+    }));
+
+    return (
+        <View style={styles.container}>
+            <Animated.Image
+                sharedTransitionTag={`image-${item.id}`}
+                sharedTransitionStyle={sharedElementTransition}
+                source={{ uri: item.image }}
+                style={{ width: '100%', height: 300 }}
+            />
+            <Animated.Text
+                entering={LightSpeedInLeft.delay(400).springify()}
+                exiting={FadeInLeft.delay(400).springify()}
+                style={{ fontWeight: 'bold', fontSize: 18, padding: 16 }}>
+                {item.title}
+            </Animated.Text>
+            <Animated.Text
+                entering={FadeInLeft.duration(300).delay(600)}
+                style={{ fontSize: 18, padding: 16 }}>
+                {item.description}
+            </Animated.Text>
+
+            {isOpen && (
+                <>
+                    <AnimatedPressable
+                        onPress={onToggle}
+                        style={styles.backdrop}
+                        entering={FadeIn}
+                        exiting={FadeOut}
+                    />
+                    <Animated.View
+                        style={[styles.sheet, translateY]}
+                        entering={SlideInDown.springify().damping(15)}
+                        exiting={SlideOutDown.duration(700)}>
+                        <Text>I am the bottom sheet</Text>
+                    </Animated.View>
+                </>
+            )}
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    backdrop: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        zIndex: 1,
+    },
+    sheet: {
+        backgroundColor: 'white',
+        zIndex: 1,
+        padding: 16,
+        height: 300,
+        width: '100%',
+        position: 'absolute',
+        bottom: -50 * 1.1,
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
+    },
+});
 
 export default Details
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,7 +1,11 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TextInput, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { HomePageProps } from '../navigation/RootStack'
-import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated'
+import { Product, data } from '../../assets/fake'
+import { sharedElementTransition } from '../utils/SharedElementTransition';
+
+const AnimatedInput = Animated.createAnimatedComponent(TextInput)
 
 const Home = ({ navigation }: HomePageProps) => {
 
@@ -10,9 +14,23 @@ const Home = ({ navigation }: HomePageProps) => {
     const backgroundColor = useSharedValue('red')
 
     const startAnimation = () => {
-        width.value = 200
-        height.value = 200
-        backgroundColor.value = 'blue'
+
+        const randomWidth = Math.floor(Math.random() * 300) + 50;
+        const randomHeight = Math.floor(Math.random() * 300) + 50;
+        const randomColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+
+        width.value = withTiming(randomWidth, {
+            duration: 1000,
+            easing: Easing.bezier(0.5, 0.01, 0, 1)
+        })
+        height.value = withTiming(randomHeight, {
+            duration: 1000,
+            easing: Easing.exp
+        })
+        backgroundColor.value = withTiming(randomColor, {
+            duration: 1000,
+            easing: Easing.linear
+        })
     }
 
     const animatedStyles = useAnimatedStyle(() => {
@@ -23,11 +41,39 @@ const Home = ({ navigation }: HomePageProps) => {
         }
     })
 
+    const animatedInputStyles = useAnimatedStyle(() => {
+        return {
+            backgroundColor: backgroundColor.value
+        }
+    })
+
     return (
-        <View>
+        <ScrollView>
+            <AnimatedInput style={[animatedInputStyles, { height: 50, margin: 10 }]} />
             <Animated.View style={animatedStyles} />
             <Button title="Start Animation" onPress={startAnimation} />
-        </View>
+            <Button title='Go to List' onPress={() => navigation.navigate('List')} />
+            {data.map((product: Product) => (
+                <View key={product.id} style={{ padding: 10, backgroundColor: 'white' }}>
+                    <TouchableOpacity
+                        style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}
+                        onPress={() => navigation.navigate('Details', { item: product })}>
+                        <Animated.Image
+                            sharedTransitionTag={`image-${product.id}`}
+                            sharedTransitionStyle={sharedElementTransition}
+                            source={{ uri: product.image }}
+                            style={{ width: 50, height: 50}}
+                        />
+                        <View style={{ flex: 1 }}>
+                            <Animated.Text style={{ fontWeight: '600', fontSize: 16, color: '#404040' }}>
+                                {product.title}
+                            </Animated.Text>
+                            <Text style={{ fontSize: 16, color: '#404040' }}>${product.price}</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            ))}
+        </ScrollView>
     )
 }
 
