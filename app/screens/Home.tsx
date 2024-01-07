f98ca204fe8e85edbb024a7229b931cd46c5a3c2
Iulia Puiu <iulia.puiu@hotmail.com>
2024-01-07 13:46:12 +0200

    Scroll to top animation

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,80 +1,122 @@
-import { View, Text, Button, TextInput, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { HomePageProps } from '../navigation/RootStack'
-import Animated, { Easing, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated'
-import { Product, data } from '../../assets/fake'
+import { View, Text, Button, TextInput, ScrollView, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { HomePageProps } from '../navigation/RootStack';
+import { Product, data } from '../../assets/fake';
+import Animated, {
+  Easing,
+  runOnJS,
+  useAnimatedReaction,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+  useSharedValue,
+  withDelay,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 import { sharedElementTransition } from '../utils/SharedElementTransition';
 
-const AnimatedInput = Animated.createAnimatedComponent(TextInput)
+const AnimatedInput = Animated.createAnimatedComponent(TextInput);
 
 const Home = ({ navigation }: HomePageProps) => {
+  const width = useSharedValue(100);
+  const height = useSharedValue(100);
+  const backgroundColor = useSharedValue('red');
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
 
-    const width = useSharedValue(100)
-    const height = useSharedValue(100)
-    const backgroundColor = useSharedValue('red')
+  const startAnimation = () => {
+    const randomWidth = Math.floor(Math.random() * 300) + 50;
+    const randomHeight = Math.floor(Math.random() * 300) + 50;
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-    const startAnimation = () => {
+    width.value = withSpring(randomWidth);
+    height.value = withSpring(randomHeight);
+    backgroundColor.value = withDelay(200, withTiming(randomColor));
+  };
 
-        const randomWidth = Math.floor(Math.random() * 300) + 50;
-        const randomHeight = Math.floor(Math.random() * 300) + 50;
-        const randomColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      width: width.value,
+      height: height.value,
+      backgroundColor: backgroundColor.value,
+    };
+  });
 
-        width.value = withTiming(randomWidth, {
-            duration: 1000,
-            easing: Easing.bezier(0.5, 0.01, 0, 1)
-        })
-        height.value = withTiming(randomHeight, {
-            duration: 1000,
-            easing: Easing.exp
-        })
-        backgroundColor.value = withTiming(randomColor, {
-            duration: 1000,
-            easing: Easing.linear
-        })
-    }
+  const animatedInputStyles = useAnimatedStyle(() => {
+    return {
+      backgroundColor: backgroundColor.value,
+    };
+  });
 
-    const animatedStyles = useAnimatedStyle(() => {
-        return {
-            width: width.value,
-            height: height.value,
-            backgroundColor: backgroundColor.value
-        }
-    })
+  const testFunction = () => {
+    console.log('I AM REACT');
+  };
 
-    const animatedInputStyles = useAnimatedStyle(() => {
-        return {
-            backgroundColor: backgroundColor.value
-        }
-    })
+  useAnimatedReaction(
+    () => {
+      console.log('EXECUTED');
+      runOnJS(testFunction)();
+      return height.value;
+    },
+    (currentValue, previousValue) => {}
+  );
 
-    return (
-        <ScrollView>
-            <AnimatedInput style={[animatedInputStyles, { height: 50, margin: 10 }]} />
-            <Animated.View style={animatedStyles} />
-            <Button title="Start Animation" onPress={startAnimation} />
-            <Button title='Go to List' onPress={() => navigation.navigate('List')} />
-            {data.map((product: Product) => (
-                <View key={product.id} style={{ padding: 10, backgroundColor: 'white' }}>
-                    <TouchableOpacity
-                        style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}
-                        onPress={() => navigation.navigate('Details', { item: product })}>
-                        <Animated.Image
-                            sharedTransitionTag={`image-${product.id}`}
-                            sharedTransitionStyle={sharedElementTransition}
-                            source={{ uri: product.image }}
-                            style={{ width: 50, height: 50}}
-                        />
-                        <View style={{ flex: 1 }}>
-                            <Animated.Text style={{ fontWeight: '600', fontSize: 16, color: '#404040' }}>
-                                {product.title}
-                            </Animated.Text>
-                            <Text style={{ fontSize: 16, color: '#404040' }}>${product.price}</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            ))}
-        </ScrollView>
-    )
-}
+  const scrollHandler = useScrollViewOffset(scrollRef);
 
-export default Home
+  const buttonStyle = useAnimatedStyle(() => {
+    console.log(scrollHandler.value);
+
+    return {
+      opacity: scrollHandler.value > 1000 ? withTiming(1) : withTiming(0),
+    };
+  });
+
+  const scrollTop = () => {
+    scrollRef.current?.scrollTo({ x: 0, y: 0, animated: true });
+  };
+
+  return (
+    <>
+      <Animated.ScrollView ref={scrollRef}>
+        <Button title="Go to List" onPress={() => navigation.navigate('List')} />
+        <AnimatedInput style={[animatedInputStyles, { height: 50, margin: 10 }]} />
+        <Animated.View style={animatedStyles} />
+        <Button title="Start Animation" onPress={startAnimation} />
+
+        {data.map((product: Product) => (
+          <View key={product.id} style={{ padding: 10 }}>
+            <TouchableOpacity
+              style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}
+              onPress={() => navigation.navigate('Details', { item: product })}>
+              <Animated.Image
+                sharedTransitionTag={`image-${product.id}`}
+                sharedTransitionStyle={sharedElementTransition}
+                source={{ uri: product.image }}
+                style={{ width: 50, height: 50 }}
+              />
+              <View style={{ flex: 1 }}>
+                <Animated.Text style={{ fontWeight: 'bold', fontSize: 18 }}>
+                  {product.title}
+                </Animated.Text>
+                <Text style={{ fontSize: 18 }}>${product.price}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </Animated.ScrollView>
+      <Animated.View style={[buttonStyle, { position: 'absolute', bottom: 20, right: 20 }]}>
+        <TouchableOpacity
+          onPress={scrollTop}
+          style={{
+            backgroundColor: '#c4c9d7',
+            borderRadius: 20,
+            padding: 10,
+          }}>
+          <Text style={{ fontSize: 24 }}>+</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </>
+  );
+};
+
+export default Home;
